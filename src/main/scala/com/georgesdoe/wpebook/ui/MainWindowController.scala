c95ec08605341baaef9f056a2b966e70da5a6a0e
GeorgeF <georgefourm@gmail.com>
2019-09-14 15:29:55 +0300

    Add concurrent execution to category fetch

--- src/main/scala/com/georgesdoe/wpebook/ui/MainWindowController.scala
+++ src/main/scala/com/georgesdoe/wpebook/ui/MainWindowController.scala
@@ -1,10 +1,14 @@
 package com.georgesdoe.wpebook.ui
 
+import java.util.concurrent.{ExecutorService, Executors}
+
 import com.georgesdoe.wpebook.wp.{Category, Client}
 import scalafx.collections.ObservableBuffer
 import scalafx.event.ActionEvent
 import scalafx.scene.control.{Button, ListView, TextField}
 import scalafxml.core.macros.sfxml
+
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutorService, Future}
 
 @sfxml
 class MainWindowController(categoryList: ListView[Category],
@@ -13,12 +17,22 @@ class MainWindowController(categoryList: ListView[Category],
 
   val categories = new ObservableBuffer[Category]()
 
+  val singleThreadedPool: ExecutorService = Executors.newSingleThreadExecutor()
+
   def handleFetch(event: ActionEvent): Unit ={
     val client = new Client(urlField.text.value)
-    val allCategories = client.fetchCategories(70)
-    categories.clear()
-    categories.addAll(allCategories)
-    categoryList.items = categories;
+
+    implicit val ec: ExecutionContextExecutorService = ExecutionContext.fromExecutorService(singleThreadedPool)
+    val fetch = Future{
+       client.fetchCategories(70)
+    }
+
+    fetch.onComplete(list => {
+      categories.clear()
+      categories.addAll(list.get)
+      categoryList.items = categories;
+    })
+
   }
 
 }
