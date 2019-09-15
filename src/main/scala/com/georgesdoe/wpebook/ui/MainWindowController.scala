e3a8e786eda7825f3b725de8adca4d58a19cb54b
GeorgeF <georgefourm@gmail.com>
2019-09-15 16:59:57 +0300

    Add posts fetching

--- src/main/scala/com/georgesdoe/wpebook/ui/MainWindowController.scala
+++ src/main/scala/com/georgesdoe/wpebook/ui/MainWindowController.scala
@@ -1,19 +1,34 @@
 package com.georgesdoe.wpebook.ui
 
-import com.georgesdoe.wpebook.wp.{Category, Client}
+import com.georgesdoe.wpebook.wp.{Category, Client, Post}
 import javafx.concurrent.{Service, Task}
+import scalafx.beans.property.StringProperty
 import scalafx.collections.ObservableBuffer
 import scalafx.event.ActionEvent
-import scalafx.scene.control.{Button, ListView, TextField}
+import scalafx.scene.control.{ListView, TableColumn, TableView, TextField}
 import scalafxml.core.macros.sfxml
 
 @sfxml
-class MainWindowController(categoryList: ListView[Category],
-                           fetchButton: Button,
-                           urlField: TextField) {
+class MainWindowController(categoryList: ListView[Category], urlField: TextField, postsTable: TableView[Post],
+                           titleColumn: TableColumn[Post, String], urlColumn: TableColumn[Post, String], dateColumn: TableColumn[Post, String]) {
 
   private val categories = new ObservableBuffer[Category]()
   categoryList.items = categories
+
+  private val posts = new ObservableBuffer[Post]()
+  postsTable.items = posts
+
+  titleColumn.cellValueFactory = cell => {
+    StringProperty(cell.value.title.rendered)
+  }
+
+  urlColumn.cellValueFactory = cell => {
+    StringProperty(cell.value.link.toString)
+  }
+
+  dateColumn.cellValueFactory = cell => {
+    StringProperty(cell.value.date.toString)
+  }
 
   private val fetchCategoriesService = new Service[List[Category]]() {
     override def createTask(): Task[List[Category]] = () => {
@@ -22,14 +37,31 @@ class MainWindowController(categoryList: ListView[Category],
     }
   }
 
+  private val fetchPostsService = new Service[List[Post]]() {
+    override def createTask(): Task[List[Post]] = () => {
+      val client = new Client(urlField.text.value)
+      client.fetchPosts(categoryList.selectionModel.value.getSelectedItem.id)
+    }
+  }
+
+  fetchPostsService.setOnSucceeded(_ => {
+    posts.clear()
+    posts.addAll(fetchPostsService.getValue)
+    fetchPostsService.reset()
+  })
+
   fetchCategoriesService.setOnSucceeded(_ => {
     categories.clear()
     categories.addAll(fetchCategoriesService.getValue)
     fetchCategoriesService.reset()
   })
 
-  def handleFetch(event: ActionEvent): Unit = {
+  def fetchCategories(event: ActionEvent): Unit = {
     fetchCategoriesService.start()
+  }
+
+  def fetchPosts(event: ActionEvent): Unit = {
+    fetchPostsService.start()
   }
 
 }
